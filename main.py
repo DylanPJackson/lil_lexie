8ff9160c851e52de27d4caabacc9c6ec4db719b8
DylanPJackson <frprdjackson@gmail.com>
2019-04-06 12:38:10 -0400

    Blank main

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+    file:
+        main.py
+
+    description:
+        Main module to for lil_lexie program
+    
+    authors:   
+        Dylan P. Jackson
+        Samuel O. Kirby
+        Justin T. Krumlauf
+        Alex J. Lamarche
+"""
+
+def main():
+    pass
+
+main()
